13003a50ef5cbcf4d1396fc79498f2741ecf4e17
Victor Alonso <vacontrerasmeza@gmail.com>
2018-08-28 19:57:40 -0500

    basics-assignment-2 finished

--- basics-assignment-2-start/src/app/app.component.ts
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  username = '';
+  username2 = '';
+  emptyUsername = false;
+
+  isEmptyUsername() {
+    // Ternary Operator equals = condition ? true : false;
+    this.emptyUsername = this.username === '' ? false : true;
+  }
+  setEmptyUsername() {
+    this.username = '';
+    this.isEmptyUsername();
+  }
 }
