8a036676aba5554aa3dcdea77770afd8206da305
Victor Alonso <vacontrerasmeza@gmail.com>
2018-09-01 00:14:02 -0500

    Assignment 3: Practicing Directives Finished

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  secretP = false;
+  i = 0;
+  timeStampValue = [];
+  log = [];
+
+  logged() {
+    this.log.push(this.log.length + 1);
+  }
+
+  getColor() {
+    if (this.i > 5) {
+      return 'blue';
+    }
+  }
 }
