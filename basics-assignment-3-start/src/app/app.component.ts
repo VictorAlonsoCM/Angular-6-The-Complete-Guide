c7c3c41558964be7605b7d16f22edb6dfede881a
Victor Alonso <vacontrerasmeza@gmail.com>
2018-09-01 00:23:18 -0500

    Getting the Index when using ngFor

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -12,7 +12,8 @@ export class AppComponent {
   log = [];
 
   logged() {
-    this.log.push(this.log.length + 1);
+    //this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 
   getColor() {
