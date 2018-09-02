a7ca3686e58e43eea794c2f108c3ab92dda46b2e
Victor Alonso <vacontrerasmeza@gmail.com>
2018-09-01 19:13:11 -0500

    Adding Content to the Recipes Components

--- chef-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ chef-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is a test', 'https://pioneerwoman.files.wordpress.com/2013/05/nachos1.jpg')
+  ];
 
   constructor() { }
 
