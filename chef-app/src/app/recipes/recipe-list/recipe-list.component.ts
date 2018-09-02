ff7bb49cea5059047ed20743b004a1d19e160282
Victor Alonso <vacontrerasmeza@gmail.com>
2018-09-01 19:59:38 -0500

    Outputting a List of Recipes with ngFor

--- chef-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ chef-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,10 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This is a test', 'https://pioneerwoman.files.wordpress.com/2013/05/nachos1.jpg')
+    new Recipe('A test Recipe', 'This is a test', 'https://pioneerwoman.files.wordpress.com/2013/05/nachos1.jpg'),
+    new Recipe('A test Recipe2', 'This is a test', 'https://pioneerwoman.files.wordpress.com/2013/05/nachos1.jpg'),
+    new Recipe('A test Recipe3', 'Lorem Ipsum es simplemente el texto cuales cont pasajes de Lorem Ipsum',
+                'https://pioneerwoman.files.wordpress.com/2013/05/nachos1.jpg')
   ];
 
   constructor() { }
