5e157caf39b44e0802925af99c50aae14008573c
Victor Alonso <vacontrerasmeza@gmail.com>
2018-12-03 16:28:45 -0600

    Lifecycle Hooks and Template Access

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,19 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,16 +21,54 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit,
+AfterViewChecked, OnDestroy {
   @Input() element: {
     type: string,
     name: string,
     content: string
   };
+  @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
 
-  constructor() { }
+  constructor() {
+    console.log('Construct');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges');
+    console.log(changes);
+  }
 
   ngOnInit() {
+    console.log('ngOnInit');
+    console.log('Text content:', this.header.nativeElement.textContent);
+  }
+
+
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentCheck');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+    console.log('Text content:', this.header.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewCheck');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
   }
 
 }
